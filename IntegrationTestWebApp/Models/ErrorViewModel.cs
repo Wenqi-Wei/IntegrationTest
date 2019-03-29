246e2db2157f00cb198e102a64b15d7126c011b1
Wenqi Wei <wenqi.wei@revolutionit.com.au>
2019-03-29 12:08:07 +1100

    removed empty json file

--- IntegrationTestWebApp/Models/ErrorViewModel.cs
+++ IntegrationTestWebApp/Models/ErrorViewModel.cs
@@ -13,5 +13,6 @@ namespace IntegrationTestWebApp.Models
         //another comment to test a new build
         //29/03/2019 changed some settings in NuGet Manager
         //29/03/2019 did a dotnet restore
+        //29/03/2019 removed the empty json file
     }
 }
