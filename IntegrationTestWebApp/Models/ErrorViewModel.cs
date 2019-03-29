076658a2b3183390f89d493863f760303c840630
Wenqi Wei <wenqi.wei@revolutionit.com.au>
2019-03-29 12:17:43 +1100

    unchecked MS local NuGet source & did dotnet restore

--- IntegrationTestWebApp/Models/ErrorViewModel.cs
+++ IntegrationTestWebApp/Models/ErrorViewModel.cs
@@ -15,5 +15,6 @@ namespace IntegrationTestWebApp.Models
         //29/03/2019 did a dotnet restore
         //29/03/2019 removed the empty json file
         //rebuilt sln solution
+        //unchecked MS local NuGet source
     }
 }
